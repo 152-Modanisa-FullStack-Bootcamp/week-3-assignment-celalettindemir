f74ea3c14a4c05de7517b794b8b5fce18c145ab4
celal258 <celal258@gmail.com>
2022-01-22 18:41:33 +0300

    Refactor Add Vuex

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,23 +1,20 @@
-import {shallowMount} from "@vue/test-utils";
+import {createLocalVue, shallowMount} from "@vue/test-utils";
 import App from "../App";
+import Vuex from "vuex";
+import {actions, getters, state} from "../store";
 
 describe("App.vue",()=>{
+    const localVue=createLocalVue();
+    localVue.use(Vuex)
     let wrapper;
     beforeEach(()=>{
         wrapper=shallowMount(App,{
-            mocks: {
-                $store: {
-                    state: {
-                        count: 0
-                    },
-                    getters:{
-                        getCount()
-                        {
-                            return this.$store.state.count;
-                        }
-                    }
-                }
-            }
+            localVue,
+            store:new Vuex.Store({
+                state,
+                getters,
+                actions
+            })
         });
     })
     it("h1 exists",()=>{
@@ -26,16 +23,16 @@ describe("App.vue",()=>{
     });
     it("h1 text equals to Daily Corona Cases in Turkey check",()=>{
         const h1 = wrapper.find("h1");
-        expect(h1.text()).toContain("Daily Corona Cases in Turkey");
+        expect(h1.text()).toBe("Daily Corona Cases in Turkey");
     });
-    it("notificationArea class check based on getCount value",()=>{
+    it("notificationArea class check based on getCount value",async ()=>{
         const notificationArea = wrapper.find(".notificationArea");
-        const count=wrapper.vm.getCount();
-        expect(notificationArea.text()).toBe(`So safe. Case count is ${count}k`)
+        const count=getters.getCount(state);
+        expect(notificationArea.text()).toContain(`Case count is ${count}k`)
     });
     it("notificationArea text message check",()=>{
         const notificationArea = wrapper.find(".notificationArea");
-        expect(notificationArea.text()).toContain("So safe. Case count is 0k")
+        expect(notificationArea.text()).toBe(wrapper.vm.message)
     });
 })
 
--- src/__tests__/Counter.spec.js
+++ src/__tests__/Counter.spec.js
@@ -1,17 +1,19 @@
-import {shallowMount} from "@vue/test-utils";
+import {createLocalVue, shallowMount} from "@vue/test-utils";
 import Counter from "../Counter";
+import Vuex from "vuex";
+import {actions, state} from "../store";
 
 describe('Counter.vue', () => {
+    const localVue=createLocalVue();
+    localVue.use(Vuex)
     let wrapper;
     beforeEach(()=>{
         wrapper=shallowMount(Counter, {
-            mocks: {
-                $store: {
-                    state: {
-                        count: 0
-                    }
-                }
-            }
+            localVue,
+            store:new Vuex.Store({
+                state,
+                actions
+            })
         });
     });
     it('Component Exist Check', () => {
@@ -19,37 +21,35 @@ describe('Counter.vue', () => {
     })
     it('Increase button exist check', () => {
         const buttons = wrapper.findAll("button");
-        expect(wrapper.exists()).toBeTruthy();
-        expect(buttons.length).toBe(2);
-        const secondButton = buttons.at(1);
+        expect(buttons).toHaveLength(2);
+        const secondButton = getButton(1);
         expect(secondButton.text()).toBe("Increase");
     })
     it('Decrease button exist check', () => {
         const buttons = wrapper.findAll("button");
-        expect(buttons.length).toBe(2);
-        const firstButton = buttons.at(0);
+        expect(buttons).toHaveLength(2);
+
+        const firstButton = getButton(0);
         expect(firstButton.text()).toBe("Decrease");
     })
     it('Increase button functionality check', () => {
-        const buttons = wrapper.findAll("button");
-        const secondButton = buttons.at(1);
+        const secondButton = getButton(1);
         const increase=jest.fn();
         wrapper.setMethods({increase:increase});
         secondButton.trigger("click");
         expect(increase).toBeCalled()
     })
     it('Decrease button functionality check', () => {
-        const buttons = wrapper.findAll("button");
-        const firstButton = buttons.at(0);
+
+        const firstButton = getButton(0);
         const decrease=jest.fn();
         wrapper.setMethods({decrease:decrease});
         firstButton.trigger("click");
         expect(decrease).toBeCalled();
     })
     it('2 increase + decrease functionality check together', () => {
-        const buttons = wrapper.findAll("button");
-        const firstButton = buttons.at(0);
-        const secondButton = buttons.at(1);
+        const firstButton = getButton(0);
+        const secondButton = getButton(1);
         const increase=jest.fn();
         const decrease=jest.fn();
         wrapper.setMethods({decrease:decrease});
@@ -64,6 +64,12 @@ describe('Counter.vue', () => {
         const span = wrapper.find("span");
         expect(span.text()).toBe("0k");
     })
+    function getButton(index)
+    {
+        const buttons = wrapper.findAll("button");
+        return buttons.at(index);
+    }
+
 })
 
 
